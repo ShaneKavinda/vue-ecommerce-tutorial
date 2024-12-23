431492fb2db892fe38732e963a44682bd75b5b67
ShaneKavinda <shanekavinda1999@gmail.com>
2024-12-23 22:54:30 +0530

    added tailwind and login form

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+import RequestPassword from "../views/RequestPassword.vue";
 
 const routes = [
     {
@@ -12,7 +14,18 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
-    }   
+    },
+    {
+        path: '/resetpassword',
+        name: 'resetpassword',
+        component: ResetPassword
+    },
+    {
+        path: '/requestpassword',
+        name: 'requestpassword',
+        component: RequestPassword
+    }      
+
 ];
 
 const router = createRouter({
--- src/views/RequestPassword.vue
+++ src/views/RequestPassword.vue
@@ -1,6 +1,12 @@
 <template>
-  <GuestLayout title="Sign in to your account">
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Request Password</h2>
+      </div>
+  
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
           <div>
             <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
@@ -26,12 +32,12 @@
           </div>
         </form>
       </div>
-  </GuestLayout>
+    </div>
 </template>
+  
 
-<script setup>
-import GuestLayout from '../components/GuestLayout.vue';
+<script>
+export default {
+    name: "RequestPassword"
+}
 </script>
-
-<style scoped>
-</style>
